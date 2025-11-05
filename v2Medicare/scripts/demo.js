0b44b90bc67496bafd362cc3cc6f147b6ec10530
IUB_LOKI <ayoubouharda33@gmail.com>
2025-11-05 14:30:34 +0100

    un demo

---
+++ v2Medicare/scripts/demo.js
@@ -0,0 +1,8 @@
+const jours =["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedu","Dimanche"];
+document.addEventListener('DOMContentLoaded',() =>
+{
+    fetch('../scripts/Doctors-v2.json')
+    .then(response => {
+        throw new err
+    })
+})
