04dc02989d3124b3e2bd735a36197e457db7c560
Yassine Sekkah <yassinesekkah11@gmail.com>
2025-11-10 16:11:05 +0100

    feat: add les function pour les buttons ajouter, modifier et supprimer

--- v2Medicare/scripts/g-medcins.js
+++ v2Medicare/scripts/g-medcins.js
@@ -1,143 +1,202 @@
-//================> Affichage de la liste de medecins <===============//
-
-
-
+/////////////////////=> njibo data mn json wen hatoha f localeStorage <=\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 fetch("../scripts/Doctors-v2.json")
-    .then(function (response) {
-        return response.json();
+    .then(function (reponse) {
+        return reponse.json();
     })
     .then(function (data) {
+        let storedDoctor = localStorage.getItem("doctors");
 
-        let storedDoctors;
-        storedDoctors = localStorage.getItem("doctors");
-
-        if (storedDoctors === null) {
+        if (storedDoctor === null) {
             localStorage.setItem("doctors", JSON.stringify(data.doctors));
-            console.log("localestorage 3amernah");
         }
         else {
-            console.log("localestorage fih data")
-
+            console.log("deja localStorage 3amer")
         }
-
-        displayDoctors()
+        displayData();
     })
-
-function displayDoctors() {
-    console.log("=== Appel de displayDoctors() ===");
-
-    let tbody = document.getElementById("medecinsTbody");
-    console.log("wach kayna", tbody)
-
+///////////////////////////////////=> daba khasna n affichiw data <=\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+function displayData() {
+    let medicinsTbody = document.getElementById("medecinsTbody");
     let doctors = JSON.parse(localStorage.getItem("doctors"));
-    console.log("jabna data mn localstorage", doctors);
 
-    tbody.innerHTML = "";
-    console.log("tbody khawia",);
-
+    medicinsTbody.innerHTML = "";
     for (let i = 0; i < doctors.length; i++) {
-        let doc = doctors[i];
-        console.log("hna traitement 3la kol medecin", i, doc);
 
         const tr = document.createElement("tr");
 
-        // name td
-        const tdName = document.createElement("td");
-        tdName.textContent = doc.name;
-        tdName.className = "border border-gray-300 p-2 text-center"
-        tr.appendChild(tdName);
+        const imageTd = document.createElement("td");
+        const img = document.createElement("img");
+        img.src = doctors[i].image;
+        img.alt = doctors[i].name;
+        img.className = "w-16 h-16 object-cover rounded-full mx-auto";
+        imageTd.className = "border border-gray-300 p-2";
+        imageTd.appendChild(img);
+        tr.appendChild(imageTd);
 
-        // speciality td
-        const tdSpec = document.createElement("td");
-        tdSpec.textContent = doc.specialty;
-        tdSpec.className = "border border-gray-300 p-2 text-center"
-        tr.appendChild(tdSpec);
+        const nameTd = document.createElement("td");
+        nameTd.textContent = doctors[i].name;
+        nameTd.className = "border border-gray-300 p-2 text-center"
+        tr.appendChild(nameTd);
 
-        // disponible td
-        const tdDispo = document.createElement("td");
-        tdDispo.textContent = doc.availabilityText;
-        tdDispo.className = "border border-gray-300 p-2 text-center"
-        tr.appendChild(tdDispo);
+        const specTd = document.createElement("td");
+        specTd.textContent = doctors[i].specialty;
+        specTd.className = "border border-gray-300 p-2 text-center"
+        tr.appendChild(specTd);
 
-        // buttons td
         const tdButton = document.createElement("td");
-        tdButton.className = "border border-gray-300 p-2 text-center"
+        tdButton.className = "border border-gray-300 p-2 text-center";
 
         const editButton = document.createElement("button");
-        editButton.textContent = "Modifier";
-        editButton.className = "text-green-700 hover:text-white  hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+        editButton.textContent = "Modifier"
+        editButton.className =
+            "text-green-700 hover:text-white hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2";
         tdButton.appendChild(editButton);
-
+        //=> l'appel de function modifier
+        editButton.addEventListener("click", function () {
+             modifierButtonFunction(doctors[i]);  
+        })
         const supButton = document.createElement("button");
         supButton.textContent = "Supprimer";
+        supButton.className =
+            "text-red-700 hover:text-white hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2";
         tdButton.appendChild(supButton);
-        supButton.className = "text-red-700 hover:text-white  hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+        //=> l'appel de function Supprimer
+        supButton.addEventListener("click", function (){
+            supprimerButtonFunction(doctors[i]);
+        })
         tr.appendChild(tdButton);
+        medicinsTbody.appendChild(tr);
 
-        tbody.appendChild(tr);
-
+        console.log("hna kol tbib bouhdo affichage dyalo", doctors[i])
     }
 }
+//////////////////////////=>nkhadmo 3la l button dyal ajouter un medecin <=\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+const ajouterModel = document.getElementById("ajouterModel");
+const ajouterDoc = document.getElementById("ajouterDoc");
+const annulerButtonForm = document.getElementById("annulerButtonMedecin");
+const ajouterButtonForm = document.getElementById("ajouterFormButton");
+///////==> input variable
+let nameInput = document.getElementById("nameInpute");
+let specInput = document.getElementById("specInpute");
+let photoInput = document.getElementById("inputPhoto");
+let descInput = document.getElementById("discriptionInput");
+let hiddenID = document.getElementById("doctorId");
+///////===> dashboard Ajouter Button
+ajouterDoc.addEventListener("click", function () {
+    hiddenID.value = "";
+    nameInput.value = "";
+    specInput.value = "";
+    photoInput.value = "";
+    descInput.value = "";
+    ajouterModel.classList.remove("hidden");  
+})
+//////// ====> annuler Button 
+annulerButtonForm.addEventListener("click", function () {
+    ajouterModel.classList.add("hidden");
+})
 
-//==========================================> AJOUTER UN MEDECIN <=========================================//
-//==>variables declaration<==//
-const ajouterButtonMedecin = document.getElementById("ajouterDoc");
-const ajouterFormMedecin = document.getElementById("ajouterModel");
-const annulerButtonMedecin = document.getElementById("annulerButtonMedecin");
-const ajouterFormButton = document.getElementById("ajouterFormButton");
+ajouterButtonForm.addEventListener("click", function () {
+    ///=> nakhdo l value dyal input 
+    let nameInputValue = nameInput.value.trim();
+    let specInputValue = specInput.value.trim();
+    let photoInputValue = photoInput.value.trim();
+    let descInputValue = descInput.value.trim();
+    
+    //=>njibo data mn localeStorage
+    let doctors = JSON.parse(localStorage.getItem("doctors")) || [];
+    //=> check wach ra7na fel modification
+    if(hiddenID.value){
+        const docId = parseInt(hiddenID.value);
+        const doctor = doctors.find((dr) => dr.id === docId);
+        
+        if(doctor){
+            doctor.name = nameInputValue;
+            doctor.specialty = specInputValue;
+            doctor.image = photoInputValue;
+            doctor.description = descInputValue;
+        }
+        localStorage.setItem("doctors", JSON.stringify(doctors));
+        displayData();
+        console.log(doctors);
 
-//==>ajouter button pour afficher le forme d'ajoute<==//
-ajouterButtonMedecin.addEventListener("click", () => ajouterFormMedecin.classList.remove("hidden"));
+        hiddenID.value = "";
+        nameInput.value = "";
+        specInput.value = "";
+        photoInput.value = "";
+        descInput.value = "";
 
-//==>annuler button pour masquer le forme d'ajoute<==//
-annulerButtonMedecin.addEventListener("click", () => ajouterFormMedecin.classList.add("hidden"));
+        ajouterButtonForm.textContent = "Ajouter"
 
-//==>ajoute buttone dans le forme pour transfere data ver localStorage<==//
-ajouterFormButton.addEventListener("click", function () {
-
-    //////////nous recuperons les donnes saisie par la secrutaire
-    const nameInpute = document.getElementById("nameInpute");
-    const specInpute = document.getElementById("specInpute");
-    const inputPhoto = document.getElementById("inputPhoto");
-    const discriptionInput = document.getElementById("discriptionInput");
-    /////////nakhdo l value dyal input 
-    const nameValue = nameInpute.value.trim();
-    const specValue = specInpute.value.trim();
-    const photoValue = inputPhoto.value.trim();
-    const discriptionValue = discriptionInput.value.trim();
-    ////////namevalue w specvalue is required
-    if (!nameValue || !specValue) {
+        ajouterModel.classList.add("hidden");
+        return;
+    }
+    //=> name et spec required
+    if (!nameInputValue || !specInputValue) {
         alert("Veuillez remplir au moins le nom et la spécialité !");
         return;
     }
-
-    ////////////// njibo data mn localeStorage
-    let doctors = JSON.parse(localStorage.getItem("doctors")) || [];
-    //////////////declaration variable newId pour le nouveau medecin dans la liste
+    //=> khasna newId ne3tiweh l newDoc
     let newId = doctors.length > 0 ? doctors[doctors.length - 1].id + 1 : 1;
-    ///////////// nsawbo l objets dyal new doctors
-    const newDoctor = {
-        id: newId,
-        name: nameValue,
-        specialty: specValue,
-        photo: photoValue,
-        description: discriptionValue,
-        availabilityText: "Non Disponible" // valeur par défaut
-    };
-    //////////npuchi lel array dyal doctor
-    doctors.push(newDoctor);
-    /////nraj3o nsaviw data flocaleStorage m3a newDoctor
-    localStorage.setItem("doctors", JSON.stringify(doctors));
-    ///////n3awdo n affichiw localStorage
-    displayDoctors();
-    //////nkhwiw lform wen7aydo affichage dyalha
-    nameInpute.value = "";
-    specInpute.value = "";
-    inputPhoto.value = "";
-    discriptionInput.value = "";
 
-    ajouterFormMedecin.classList.add("hidden");
-    console.log(doctors.doctors);
+    //=> ncriyiw object jdid l newDoc
+    let newDoc = {
+        id: newId,
+        name: nameInputValue,
+        specialty: specInputValue,
+        image: photoInputValue,
+        description: descInputValue
+    }
+    //=>pushi newDoc l doctors lihia data dyalna 
+    doctors.push(newDoc);
+
+    //=> nsifto doctors l localeStorage
+    localStorage.setItem("doctors", JSON.stringify(doctors));
+    displayData();
+
+    //=> khwi l input
+    nameInput.value = "";
+    specInput.value = "";
+    photoInput.value = "";
+    descInput.value = "";
+    //=> sad lforme
+    ajouterModel.classList.add("hidden");
+
+    console.log(doctors)
 })
+/////////////////////// daba MODIFIER function 
+function modifierButtonFunction(doc){
+        ajouterModel.classList.remove("hidden");
+        document.getElementById("nameInpute").value = doc.name;
+        document.getElementById("specInpute").value = doc.specialty;
+        document.getElementById("inputPhoto").value = doc.image;
+        document.getElementById("discriptionInput").value = doc.description;
+
+        document.getElementById("doctorId").value = doc.id;
+        
+        document.getElementById("ajouterFormButton").textContent = "Modifier";
+    }
+///////////////////////////// daba khdam 3la SUPPRIMER
+    function supprimerButtonFunction(doc){
+        let doctors = JSON.parse(localStorage.getItem("doctors")) || [];
+        console.log("ttttt", doctors);
+        let index = doctors.findIndex(dr => dr.id === doc.id);
+        
+        if(index !== -1){
+            doctors.splice(index, 1);
+            localStorage.setItem("doctors", JSON.stringify(doctors)) || [];
+            displayData();
+        }
+    }
+
+    ///// bach nfermiw l forme becklick bera
+    ajouterModel.addEventListener("click", function (e){
+        if(e.target === ajouterModel){
+            ajouterModel.classList.add("hidden");
+        }
+    })
+    localStorage.clear()
+  
+
+
 
 
