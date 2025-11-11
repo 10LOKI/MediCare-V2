dd3ae69ba4cdda41bafd7408e6759ffd8fec36c7
IUB_LOKI <ayoubouharda33@gmail.com>
2025-11-11 14:49:48 +0100

    update

--- v2Medicare/scripts/dispo2.0.js
+++ v2Medicare/scripts/dispo2.0.js
@@ -1,4 +1,27 @@
-document.addEventListener('DOMContentLoaded',()=>
+fetch("../scripts/Doctors-v2.json")
+    .then(function (reponse) {
+        return reponse.json();
+    })
+    .then(function (data) {
+        let storedDoctor = localStorage.getItem("doctors");
+
+        if (storedDoctor === null) {
+            localStorage.setItem("doctors", JSON.stringify(data.doctors));
+        }
+        else {
+            console.log("deja localStorage 3amer")
+        }
+        fillSpecSelect();
+        displayData();
+    })
+
+    .then(jsonData =>
+    {
+        const container = document.getElementById('medcinCartes');
+        
+    }
+    )
+function dispoConnect()
 {
-    fetch('../v1Medicare')
-})
+    const choiDispo = document.getElementById
+}
