83902cd2302a3f9c42008a2f41c253b699636cf4
unknown <outergamoustafa@gmail.com>
2025-11-12 20:27:46 +0100

    fix: fixed problem filter was wroten 'filtre'

--- admin/scripts/g_rdv.js
+++ admin/scripts/g_rdv.js
@@ -8,6 +8,47 @@
 //     }        
 // }
 
+// *********** filtre par rdv accepted *********
+const green = document.querySelector(".acceptRdvBtn");
+green.addEventListener('click', () => {
+
+    const arrayRdv = JSON.parse(localStorage.getItem('appointments') || '[]')
+    const Green = arrayRdv.filter(item => item.statusRdv === "accepté")
+    htmlTable(Green)
+})
+
+// *********** filtre par rdv refuse *********
+const red = document.querySelector(".refuseRdvBtn");
+red.addEventListener('click', () => {
+
+    const arrayRdv = JSON.parse(localStorage.getItem('appointments') || '[]')
+    const Red = arrayRdv.filter(item => item.statusRdv === "refusé")
+    htmlTable(Red)
+})
+
+let listDisponibilite = document.querySelector('.disponible')
+listDisponibilite.addEventListener('click', () => {
+    listDisponibilite.innerHTML = `
+<div class="w-48 text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    <button type="button" class="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b border-gray-200 rounded-t-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+        <svg class="w-3 h-3 me-2.5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+        </svg>
+        Profile
+    </button>
+    <button type="button" class="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+        <svg class="w-3 h-3 me-2.5" aria-hidden="true" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.75 4H19M7.75 4a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 4h2.25m13.5 6H19m-2.25 0a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 10h11.25m-4.5 6H19M7.75 16a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 16h2.25"/>
+        </svg>
+        Settings
+    </button>
+</div>
+
+`
+})
+
+
+
 function htmlTable(table) {
     const listRendezVous = document.querySelector("#tbody")
     listRendezVous.innerHTML = table.map(item => `
@@ -18,13 +59,13 @@ function htmlTable(table) {
             
             <td class="flex flex-col md:flex-row items-center md:justify-evenly">
                 <p class="flex items-center justify-center cursor-pointer w-8 h-14">
-                    <button onclick="acceptRdv(${item.id})" class="hover:-rotate-180 w-full h-8 rounded-full bg-center bg-no-repeat bg-[url('../../v1Medicare/images/verifie.png')] duration-500"></button>
+                    <button onclick="acceptRdv(${item.id})" class="hover:-rotate-180 w-full h-8 rounded-full bg-center bg-no-repeat bg-[url('../../images/verifie.png')] duration-500"></button>
                 </p>
 
                 <p class="flex items-center justify-center  w-20 text-sm font-extrabold duration-600 transition-all">${item.statusRdv}</p>
 
                 <p onclick="refusRdv(${item.id})" class="flex items-center justify-center cursor-pointer  w-8 h-14">
-                    <button class="hover:rotate-180 w-full h-8 rounded-full bg-center bg-no-repeat bg-[url('../../v1Medicare/images/supprimer.png')] duration-500"></button>
+                    <button class="hover:rotate-180 w-full h-8 rounded-full bg-center bg-no-repeat bg-[url('../../images/supprimer.png')] duration-500"></button>
                 </p>
             </td>
         </tr>
@@ -64,18 +105,5 @@ function acceptRdv(idState) {
     }
 
 }
-
-// *********** filtre par rdv accepted *********
-const green = document.querySelector(".green")
-green.addEventListener('click', () => {
-    const arrayRdv = JSON.parse(localStorage.getItem('appointments') || '[]')
-
-
-    let Green = arrayRdv.forEach(element => {
-        element.statusRdv == 'accepté'
-    htmlTable(Green)})
-
-
-})
 
 listRendezVous()
