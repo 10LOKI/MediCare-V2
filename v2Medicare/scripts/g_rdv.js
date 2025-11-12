02fea160e7e92a0296102f00b3d657a89997e563
unknown <outergamoustafa@gmail.com>
2025-11-12 10:55:38 +0100

    feat: mis'ajour par sram-master

--- v2Medicare/scripts/g_rdv.js
+++ v2Medicare/scripts/g_rdv.js
@@ -8,11 +8,9 @@
 //     }        
 // }
 
-function listRendezVous(){
-    const arrayRdv = JSON.parse(localStorage.getItem('appointments') || '[]')
-
-    const listRendezVous = document.querySelector("#tbody")
-    listRendezVous.innerHTML = arrayRdv.map(item => `
+function htmlTable(table){
+     const listRendezVous = document.querySelector("#tbody")
+    listRendezVous.innerHTML = table.map(item => `
         <tr class="border-b-8 text-lg text-blue-950 bg-blue-600/50">
             <td class="px-1 md:px-3 lg:px-6 py-3">${item.fullName}</td>
             <td class="px-1 md:px-3 lg:px-6 py-3">${item.doctors}</td>
@@ -24,20 +22,23 @@ function listRendezVous(){
                     <button onclick="acceptRdv(${item.id})" class="hover:-rotate-180 w-full h-8 rounded-full bg-center bg-no-repeat bg-[url('../../v1Medicare/images/verifie.png')] duration-500"></button>
                 </p>
 
-                <p id="status"
-                class="flex items-center justify-center  w-20 text-sm font-extrabold duration-600 transition-all">${item.statusRdv}</p>
+                <p class="flex items-center justify-center  w-20 text-sm font-extrabold duration-600 transition-all">${item.statusRdv}</p>
 
-                <p onclick="refusRdv(${item.id})"
-                class="flex items-center justify-center cursor-pointer  w-8 h-14">
-                <button class="hover:rotate-180 w-full h-8 rounded-full bg-center bg-no-repeat bg-[url('../../v1Medicare/images/supprimer.png')] duration-500"></button>
+                <p onclick="refusRdv(${item.id})" class="flex items-center justify-center cursor-pointer  w-8 h-14">
+                    <button class="hover:rotate-180 w-full h-8 rounded-full bg-center bg-no-repeat bg-[url('../../v1Medicare/images/supprimer.png')] duration-500"></button>
                 </p>
-        </td>
+            </td>
         </tr>
     `).join('');
-}   
-listRendezVous()
+}
+
+function listRendezVous(){
+    const arrayRdv = JSON.parse(localStorage.getItem('appointments') || '[]')
+    htmlTable(arrayRdv)
+}
 // let statusRdv = document.getElementById("status") nothing with that, i call him by id.statusRdv that aleardy passed it works with click the button
 
+//********** rdv  refused *************/
 function refusRdv(idState){
     const arrayRdv = JSON.parse(localStorage.getItem('appointments') || '[]')
 
@@ -50,13 +51,13 @@ function refusRdv(idState){
     }
 
 }
-listRendezVous()
 
+//************ Rendez-vous accepted *************/
 function acceptRdv(idState){
     const arrayRdv = JSON.parse(localStorage.getItem('appointments') || '[]')
 
     if (confirm('Êtes-vous sûr de Accepté ce rendez-vous ?')) {
-        acceptAppointments = arrayRdv.map(apt =>
+       let  acceptAppointments = arrayRdv.map(apt =>
             apt.id === idState ? { ...apt, statusRdv: 'accepté' } : apt
         );
         localStorage.setItem('appointments', JSON.stringify(acceptAppointments))
@@ -64,6 +65,18 @@ function acceptRdv(idState){
     }
     
 }
+
+// *********** filtre par rdv accepted *********
+const green = document.querySelector(".green")
+green.addEventListener('click', ()=>{
+    const arrayRdv = JSON.parse(localStorage.getItem('appointments') || '[]')
+
+    
+    let Green = arrayRdv.filtre(item=> item.statusRdv == 'accepté')
+    htmlTable(Green)
+
+})
+
 listRendezVous()
 
 
